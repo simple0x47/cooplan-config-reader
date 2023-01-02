462f91bd4fb5e37c9c4d6e8dbade3ec98be0cad8
Gabriel Ami <gabriel@simpleg.eu>
2023-01-02 18:41:39 +0200

    + basic json file reader

--- src/reader.rs
+++ src/reader.rs
@@ -1,21 +1,59 @@
 use std::io::{Error, ErrorKind};
 use serde::Deserialize;
+use serde_json::Value;
 
-pub async fn try_read<T: Deserialize>(file_path: &str) -> Result<T, Error> {
+pub async fn try_read<T: for<'de> Deserialize<'de>>(file_path: &str) -> Result<T, Error> {
     let value = match tokio::fs::read_to_string(file_path).await {
-        Ok(value) => match serde_json::from_str::<Api>(&value) {
-            Ok(value) => value,
-            Err(error) => {
-                return Err(Error::new(
-                    ErrorKind::InvalidData,
-                    format!("failed to deserialize api file's content: {}", error),
-                ));
-            }
-        },
+        Ok(file_content) => try_deserialize(file_content)?,
         Err(error) => {
             return Err(error);
         }
     };
 
     Ok(value)
+}
+
+fn try_deserialize<T: for<'de> Deserialize<'de>>(file_content: String) -> Result<T, Error> {
+    match serde_json::from_str::<Value>(&file_content) {
+        Ok(value) => match serde_json::from_value::<T>(value) {
+            Ok(value) => Ok(value),
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("failed to deserialize file's content: {}", error),
+                ));
+            }
+        },
+        Err(error) => {
+            Err(Error::new(
+                ErrorKind::InvalidData,
+                format!("failed to deserialize file's content: {}", error),
+            ))
+        }
+    }
+}
+
+#[cfg(test)]
+#[test]
+pub fn converts_string_into_expected_value() {
+    #[derive(Deserialize)]
+    struct Example {
+        a: u32,
+        b: String,
+        c: bool,
+    }
+
+    let first_json = r#"{"a": 1, "b": "2", "c": true}"#;
+    let second_json = r#"{"a": 3, "b": "4", "c": false}"#;
+
+    let first = try_deserialize::<Example>(first_json.to_string()).unwrap();
+    let second = try_deserialize::<Example>(second_json.to_string()).unwrap();
+
+    assert_eq!(first.a, 1);
+    assert_eq!(first.b, "2");
+    assert_eq!(first.c, true);
+
+    assert_eq!(second.a, 3);
+    assert_eq!(second.b, "4");
+    assert_eq!(second.c, false);
 }
